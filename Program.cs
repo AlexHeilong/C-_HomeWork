cbdf103f72c8016468cc71c981d0f29b016b4d34
AlexHeilong <alexdeev@mail.ru>
2022-09-06 00:16:52 +0800

    seminar 9  homework added

--- Program.cs
+++ Program.cs
@@ -1150,7 +1150,7 @@ int size = Convert.ToInt32(Console.ReadLine());
 Show2dArray(CreateSpiralArray(size));
 */
 
-
+/*
 Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 
@@ -1189,6 +1189,92 @@ void PrintArray(int[,] array)
         Console.WriteLine("");
     }
 }
+*/
+
+//_______________________________________________
+
+// Домашняя работа к семинару 9.
+
+// Task 1.
+//Задача 64: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+/*
+void AllNaturals(int m, int n)
+{
+    if(m > n)
+    {
+        int temp = n;
+        n = m;
+        m = temp;
+    }
+    
+    if(m != n) AllNaturals(m, n-1);
+        Console.Write(n + " ");
+}
+
+Console.WriteLine("Input number m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+AllNaturals(m, n);
+*/
+
+// Task 2.
+// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+/*
+int SumOfNaturals(int m, int n)
+{
+    if(m > n)
+    {
+        int temp = n;
+        n = m;
+        m = temp;
+    }
+    if(m != n) return n + SumOfNaturals(m, n-1);
+    else return m;
+}
+
+Console.WriteLine("Input number m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма чисел в промежутке {m} и {n} =  {SumOfNaturals(m, n)}");
+*/
+
+// Task 3.
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+
+
+int Ak(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (n == 0 && m > 0)
+    {
+        return Ak(m - 1, 1);
+    }
+    else
+    {
+        return Ak(m - 1, Ak(m, n - 1));
+    }
+}
+
+Console.WriteLine("Input number m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"m = {m} и n = {n} =  {Ak(m, n)}");
 
 
 
